a7c99c1675b4dd903f611b1be2996d527ffb6305
Siddhartha Basu <biosidd@gmail.com>
2022-09-29 11:39:12 -0500

    feat: add steampipe

--- nix/go-packages.nix
+++ nix/go-packages.nix
@@ -1,4 +1,4 @@
-{ lib, buildGo118Module, fetchFromGitHub }:
+{ lib, buildGo118Module, fetchFromGitHub, installShellFiles }:
 
   {
 
@@ -230,4 +230,40 @@
 	    maintainers = with maintainers; [ cybersiddhu ];
 	};
      };
+     steampipe = buildGo118Module rec {
+	  pname = "steampipe";
+	  version = "0.15.4";
+
+	  src = fetchFromGitHub {
+	    owner = "turbot";
+	    repo = "steampipe";
+	    rev = "v${version}";
+	    sha256 = "sha256-QBmXWsAJepjQqfAaE8K2wA7Qi6HEDzqUECZkEPWAYQY=";
+	  };
+
+	  vendorSha256 = "sha256-/d1wdJw9DGOvFpQYzNJF9xuy36xF/ZKxxlOs1EFtR14=";
+	  proxyVendor = true;
+
+	  nativeBuildInputs = [ installShellFiles ];
+
+	  ldflags = [
+	    "-s"
+	    "-w"
+	  ];
+
+	  postInstall = ''
+	    INSTALL_DIR=$(mktemp -d)
+	    installShellCompletion --cmd steampipe \
+	      --bash <($out/bin/steampipe --install-dir $INSTALL_DIR completion bash) \
+	      --fish <($out/bin/steampipe --install-dir $INSTALL_DIR completion fish) \
+	      --zsh <($out/bin/steampipe --install-dir $INSTALL_DIR completion zsh)
+	  '';
+
+	  meta = with lib; {
+	    homepage = "https://steampipe.io/";
+	    description = "select * from cloud;";
+	    license = licenses.agpl3;
+	    maintainers = with maintainers; [ hardselius ];
+	  };
+	};
 }
