15c30025e35883356d67ba373d22ecbc88cc9304
Siddhartha Basu <biosidd@gmail.com>
2023-05-10 16:28:50 -0500

    feat: add clusterctl nix derivation

--- nix/go-more-packages.nix
+++ nix/go-more-packages.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, fetchFromGitHub, stdenv }:
+{ lib, buildGoModule, fetchFromGitHub, stdenv , installShellFiles }:
 
 {
 	terraform-ls = buildGoModule rec {
@@ -53,6 +53,82 @@
 	    homepage = "https://github.com/gofireflyio/aiac"; 
 	    license = licenses.asl20;
 	    maintainers = with maintainers; [ cybersiddhu ]; 
+	  };
+	};
+
+	k0sctl = buildGoModule rec {
+	  pname = "k0sctl";
+	  version = "0.15.0";
+
+	  src = fetchFromGitHub {
+	    owner = "k0sproject";
+	    repo = pname;
+	    rev = "v${version}";
+	    sha256 = "sha256-i/XgEPuYNxn10eOXfF+X33oLlkO9r6daeygZcSdcicQ=";
+	  };
+
+	  vendorSha256 = "sha256-RTC2AEDzSafvJT/vuPjzs25PhuzBiPb32an/a/wpY04=";
+
+	  ldflags = [
+	    "-s"
+	    "-w"
+	    "-X github.com/k0sproject/k0sctl/version.Environment=production"
+	    "-X github.com/k0sproject/k0sctl/version.Version=${version}"
+	  ];
+
+  	  nativeBuildInputs = [ installShellFiles ];
+
+	  postInstall = ''
+	    for shell in bash zsh fish; do
+	      installShellCompletion --cmd ${pname} \
+		--$shell <($out/bin/${pname} completion --shell $shell)
+	    done
+	  '';
+
+	  meta = with lib; {
+	    description = "A bootstrapping and management tool for k0s clusters.";
+	    homepage = "https://k0sproject.io/";
+	    license = licenses.asl20;
+	    maintainers = with maintainers; [ nickcao ];
+	  };
+	};
+
+	clusterctl = buildGoModule rec {
+  	  pname = "clusterctl";
+  	  version = "1.4.2";
+
+	  src = fetchFromGitHub {
+	    owner = "kubernetes-sigs";
+	    repo = "cluster-api";
+	    rev = "v${version}";
+	    sha256 = "sha256-NvCQs6YzQ2zNLQiYgFK/q2c74g/+YkzQIQJWEINOYIE=";
+	  };
+
+	  vendorSha256 = "sha256-ZJFpzBeC048RZ6YfjXQPyohCO1WagxXvBBacifkfkjE=";
+
+	  subPackages = [ "cmd/clusterctl" ];
+
+	  nativeBuildInputs = [ installShellFiles ];
+
+	  ldflags = let t = "sigs.k8s.io/cluster-api/version"; in [
+	    "-X ${t}.gitMajor=${lib.versions.major version}"
+	    "-X ${t}.gitMinor=${lib.versions.minor version}"
+	    "-X ${t}.gitVersion=v${version}"
+	  ];
+
+	  postInstall = ''
+	    export HOME=$TMPDIR
+
+	    installShellCompletion --cmd clusterctl \
+	      --bash <($out/bin/clusterctl completion bash) \
+	      --zsh <($out/bin/clusterctl completion zsh)
+	  '';
+
+	  meta = with lib; {
+	    description = "Kubernetes cluster API tool";
+	    homepage = "https://cluster-api.sigs.k8s.io/";
+	    license = licenses.asl20;
+	    maintainers = with maintainers; [ zowoq ];
 	  };
 	};
 }
